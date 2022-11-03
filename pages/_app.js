3c55d1972e5fe587e3b2c3f69b1a2253868133c9
Sangmin <23mean@naver.com>
2022-11-04 02:54:32 +0900

    Feat: _app.js내 pageProps를 통한 SEO처리

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,38 @@
-import CarContextWrapper from 'context/CarContext';
-import NavContextWrapper from 'context/NavContext';
-import { ThemeProvider } from 'styled-components';
+import CarContextWrapper from "context/CarContext";
+import NavContextWrapper from "context/NavContext";
+import { ThemeProvider } from "styled-components";
+import Head from "next/head";
+import { DefaultSeo } from "next-seo";
 
-import GlobalStyle from 'styles/GlobalStyle';
-import Theme from 'styles/Theme';
+import { DEFAULT_SEO } from "constants/defaultSEO";
+
+import SEO from "components/common/SEO";
+
+import GlobalStyle from "styles/GlobalStyle";
+import Theme from "styles/Theme";
+
 function MyApp({ Component, pageProps }) {
+  const isEmptyObj = (obj) => {
+    if (obj.constructor === Object && Object.keys(obj).length === 0) {
+      return true;
+    }
+    return false;
+  };
+
   return (
     <>
+      <Head>
+        <title>알티모빌리티</title>
+        <meta name="description" content="알티모빌리티" />
+        <link rel="icon" href="/알티모빌리티.jpeg" />
+      </Head>
+
+      {isEmptyObj(pageProps) ? (
+        <DefaultSeo {...DEFAULT_SEO} />
+      ) : (
+        <SEO data={pageProps} />
+      )}
+
       <NavContextWrapper>
         <CarContextWrapper>
           <GlobalStyle />
