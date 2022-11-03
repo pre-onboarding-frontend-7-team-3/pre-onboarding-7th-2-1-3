e21416ada5705a4572d6a442fc17dce8b771c445
YeonghunKO <yhko1988@gmail.com>
2022-11-03 21:45:13 +0900

    Feat : _App 에 기본적인 세팅을 해줌

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
-import '../styles/globals.css'
+import { ThemeProvider } from 'styled-components';
 
+import App from './App';
+
+import GlobalStyle from 'styles/GlobalStyle';
+import Theme from 'styles/Theme';
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <GlobalStyle />
+      <ThemeProvider theme={Theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
